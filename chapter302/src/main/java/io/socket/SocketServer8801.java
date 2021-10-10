5caefeade7670a3d6337a0b0d77d0c8fad79490e
simonzhang <simonzhang@nuskin.com>
2021-10-10 14:27:15 +0800

    commit chapter3 second question

---
+++ chapter302/src/main/java/io/socket/SocketServer8801.java
@@ -0,0 +1,48 @@
+package io.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer8801 {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8801);
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                // 多线程处理请求（但是线程未复用）
+                new Thread(() -> {
+                    service(socket);
+                }).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void service(Socket socket) {
+        try {
+
+            InputStream inputStream = socket.getInputStream();
+            byte[] buf = new byte[1024];
+            inputStream.read(buf);
+            System.out.println("Request Header Info:" + new String(buf));
+
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "Hello~ This is the first NIO Project!";
+            // 必须要指定长度，否则Client不知道返回的文本到何处结束，会读取出错。
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
