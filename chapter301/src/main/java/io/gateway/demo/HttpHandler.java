17b48799216ea3e7f7fa9eb799b7886ca94afa6d
simonzhang <simonzhang@nuskin.com>
2021-10-10 12:59:17 +0800

    commit chapter3 first question

---
+++ chapter301/src/main/java/io/gateway/demo/HttpHandler.java
@@ -0,0 +1,119 @@
+package io.gateway.demo;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.util.ReferenceCountUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class HttpHandler extends ChannelInboundHandlerAdapter {
+    
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            //logger.info("接收到的请求url为{}", uri);
+            if (uri.contains("/test")) {
+                handlerTest(fullRequest, ctx, "hello, this is just a test~");
+            } else {
+                handlerTest(fullRequest, ctx);
+            }
+    
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx, String... body) {
+        FullHttpResponse response = null;
+        try {
+            String value = null;
+            if (null != body && body.length > 0) {
+                value = body[0];
+            } else {
+                value = httpClientTest("Http://localhost:8801");
+            }
+
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+
+        } catch (Exception e) {
+            System.out.println("处理出错:"+e.getMessage());
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+                ctx.flush();
+            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    private String httpClientTest(String Url) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet httpGet = new HttpGet(Url);
+        String result = "empty~";
+
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+            HttpEntity responseEntity = response.getEntity();
+            if (responseEntity != null) {
+                result = EntityUtils.toString(responseEntity);
+            }
+        } catch (IOException e) {
+            System.out.println("Connect failed:"+e.getMessage());
+        } finally {
+            try {
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+}
